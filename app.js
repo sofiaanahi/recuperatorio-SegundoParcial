a726c2f1d2c7832554c85d9eeec59f0d9a153026
sofiaanahi <sofiaanahirodas@gmail.com>
2023-07-05 14:41:26 -0300

    se completo el app.js y se creo el .env

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const morgan = require('morgan');
 const path = require('path');
+const helmet = require('helmet');
+
+require('dotenv').config();
+require('ejs')
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 // Middlewares
 // TODO: Implementar middlewares
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+});
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on port ${port}`));
